d7806559f9d3ad9dbed1b147b75c139036ad04f1
kv2461 <kevinarnoldtv@gmail.com>
2022-05-13 14:35:22 -0700

    added dotenv

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose'; 
+import dotenv from 'dotenv';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 
 
@@ -15,10 +17,8 @@ app.use(cors());
 
 app.use('/posts',postRoutes); //needs to be below cors
 
-const CONNECTION_URL = `mongodb+srv://kv2461:LTYGr2zzy86W21Hw@cluster0.htfej.mongodb.net/tutorial_data?retryWrites=true&w=majority`
-
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser:true, useUnifiedTopology:true})//for simplistic purposes, avoids errors
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser:true, useUnifiedTopology:true})//for simplistic purposes, avoids errors
     .then(()=>app.listen(PORT,()=>console.log(`Server is running on port: ${PORT}`)))
     .catch((error)=>console.log(error));
