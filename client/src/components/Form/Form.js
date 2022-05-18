53d92b055896445292b6c8584209f0cdd9e65360
kv2461 <kevinarnoldtv@gmail.com>
2022-05-18 15:31:02 -0700

    added rerouting when creating new post to its details

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,6 +2,7 @@ import React ,{ useState, useEffect } from 'react';
 import {TextField, Button, Typography, Paper} from '@material-ui/core';
 import FileBase from 'react-file-base64'; //for selectedFile
 import { useDispatch,useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import useStyles from './styles';
 import { createPost,updatePost } from '../../actions/posts';
@@ -11,6 +12,7 @@ const Form = ( { currentId, setCurrentId }) => {
     const post = useSelector((state)=>currentId?state.posts.posts.find((p)=> p._id===currentId): null);
     const classes = useStyles();
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const user = JSON.parse(localStorage.getItem('profile'));
 
     useEffect(() => {
@@ -27,7 +29,7 @@ const Form = ( { currentId, setCurrentId }) => {
         e.preventDefault()//prevents refresh in browser
 
         if (currentId === 0) {
-            dispatch(createPost({...postData,name: user?.result?.name}));
+            dispatch(createPost({...postData,name: user?.result?.name}, navigate));
             clear();
         } else {
             dispatch(updatePost(currentId,{...postData, name: user?.result?.name }));
